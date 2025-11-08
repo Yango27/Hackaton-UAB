f93b1e987d2e89e11a17ea75307d6a2838bdda36
Yango <yangokun27@gmail.com>
2025-11-08 13:51:39 +0100

    apanalysis change

--- data_analysis/APanalysis.py
+++ data_analysis/APanalysis.py
@@ -1,5 +1,3 @@
 import auxFunctions as aux
 apsDF = aux.load_aps(r"anonymized_data\aps")
-clientsDF = aux.load_clients(r"anonymized_data\clients")
 aux.print_dataset_summary(apsDF)
-aux.print_dataset_summary(clientsDF)
