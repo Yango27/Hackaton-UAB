6d2ca8f3221947fb3758c3d51e86faa4c0d9cc0d
Yango <yangokun27@gmail.com>
2025-11-08 13:46:55 +0100

    data analysis

--- data_analysis/APanalysis.py
+++ data_analysis/APanalysis.py
@@ -1,1 +1,5 @@
-import pandas as pd
+import auxFunctions as aux
+apsDF = aux.load_aps(r"anonymized_data\aps")
+clientsDF = aux.load_clients(r"anonymized_data\clients")
+aux.print_dataset_summary(apsDF)
+aux.print_dataset_summary(clientsDF)
